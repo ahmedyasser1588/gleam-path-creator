e03034ee2761739301c4892ee41e1849cd1e6375
Ahmed Yasser <ay1588@fayoum.edu.eg>
2026-04-18 20:40:18 +0200

    UPDATE

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -12,7 +12,7 @@ import EsoBot from "@/components/EsoBot";
 import FlowerGarden from "@/components/FlowerGarden";
 
 // التاريخ والوقت المظبوط
-const BIRTHDAY = new Date("2026-04-18T20:40:00");
+const BIRTHDAY = new Date("2026-04-19T12:12:00");
 
 const Index = () => {
   const [currentPage, setCurrentPage] = useState(0);
